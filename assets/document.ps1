e11bc44179cd7fba7e5d7be54b152b41ee14b006
George Olson <georgeo@slalom.com>
2017-05-18 06:21:17 -0700

    Update to 1.8.12

--- assets/document.ps1
+++ assets/document.ps1
@@ -1,27 +1,27 @@
 cd $PSScriptRoot
 
 if (!(Test-Path "$PSScriptRoot\Document.exe")) {
-    $url = "https://github.com/slalom-saa/stacks/raw/develop/assets/Document.exe"
+    $url = "https://slalomstacks.blob.core.windows.net/public/Document.exe"
     $output = "$PSScriptRoot\Document.exe"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
 }
 if (!(Test-Path "$PSScriptRoot\Microsoft.Build.dll")) {
-    $url = "https://github.com/slalom-saa/stacks/raw/develop/assets/Microsoft.Build.dll"
+    $url = "https://slalomstacks.blob.core.windows.net/public/Microsoft.Build.dll"
     $output = "$PSScriptRoot\Microsoft.Build.dll"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
 }
 if (!(Test-Path "$PSScriptRoot\Microsoft.Build.Framework.dll")) {
-    $url = "https://github.com/slalom-saa/stacks/raw/develop/assets/Microsoft.Build.Framework.dll"
+    $url = "https://slalomstacks.blob.core.windows.net/public/Microsoft.Build.Framework.dll"
     $output = "$PSScriptRoot\Microsoft.Build.Framework.dll"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
 }
 if (!(Test-Path "$PSScriptRoot\Microsoft.Build.Tasks.Core.dll")) {
-    $url = "https://github.com/slalom-saa/stacks/raw/develop/assets/Microsoft.Build.Tasks.Core.dll"
+    $url = "https://slalomstacks.blob.core.windows.net/public/Microsoft.Build.Tasks.Core.dll"
     $output = "$PSScriptRoot\Microsoft.Build.Tasks.Core.dll"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
 }
 if (!(Test-Path "$PSScriptRoot\Microsoft.Build.Utilities.Core.dll")) {
-    $url = "https://github.com/slalom-saa/stacks/raw/develop/assets/Microsoft.Build.Utilities.Core.dll"
+    $url = "https://slalomstacks.blob.core.windows.net/public/Microsoft.Build.Utilities.Core.dll"
     $output = "$PSScriptRoot\Microsoft.Build.Utilities.Core.dll"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
 }
